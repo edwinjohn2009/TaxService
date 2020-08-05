946d657ecebe1d3d4b4f535821a77e12309bf620
edwinjohn2009 <69232640+edwinjohn2009@users.noreply.github.com>
2020-08-05 01:38:15 -0400

    Add files via upload

---
+++ ICanadaCustomer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaxService.Interfaces
+{
+    interface ICanadaCustomer
+    {
+        Task GetTaxRatesBasedOnCanada();
+
+        Task CalculateTaxesForAnOrderFromCanada();
+    }
+}
---
+++ IUSCustomer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaxService.Interfaces
+{
+    interface IUSCustomer
+    {
+        Task GetTaxRatesBasedOnUS();
+
+        Task CalculateTaxesForAnUSOrder();
+    }
+}
